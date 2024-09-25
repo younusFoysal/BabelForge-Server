96006e48b23e3be2c093b089dc9518ad807b552e
nhnaahid <nhnahid.iu@gmail.com>
2024-09-25 17:39:34 +0600

    fix: hope it okay now. after reset

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
-     "https://babel-forge.vercel.app"
+    "https://babel-forge.vercel.app"
   ],
   credentials: true,
   optionSuccessStatus: 200,
