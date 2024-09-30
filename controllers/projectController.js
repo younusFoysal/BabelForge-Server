e1d769bf44485f543e7dce3944fa1d8eb43077d2
morshidulrahman <boydanger416@gmail.com>
2024-09-30 12:53:23 +0600

    search and get api

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,4 +1,9 @@
-const { addProjects, deleteProjects } = require("../services/projectService");
+const {
+  addProjects,
+  deleteProjects,
+  getAllProjects,
+  searchProject,
+} = require("../services/projectService");
 
 const addPoject = async (req, res) => {
   const db = req.app.locals.db;
@@ -7,12 +12,23 @@ const addPoject = async (req, res) => {
   res.send(result);
 };
 
+const getProjects = async (req, res) => {
+  const db = req.app.locals.db;
+  const name = req.query.name;
+  let result;
+  if (name) {
+    result = await searchProject(db, name);
+  } else {
+    result = await getAllProjects(db);
+  }
+  res.send(result);
+};
+
 const deleteProject = async (req, res) => {
   const db = req.app.locals.db;
   const projectId = req.params.id;
   const result = await deleteProjects(db, projectId);
   res.send(result);
+};
 
-}
-
-module.exports = { addPoject , deleteProject};
+module.exports = { addPoject, deleteProject, getProjects };
