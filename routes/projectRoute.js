2eec0377790d1f16a24bc4440083096e94528a2b
tofayeLL <ahmedtofayel132@gmail.com>
2024-09-30 12:31:08 +0600

    Project page: Delete Api created

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { addPoject } = require("../controllers/projectController");
+const { addPoject, deleteProject } = require("../controllers/projectController");
 
 router.post("/projects", addPoject);
+
+router.delete("/projects/:id", deleteProject);
 
 module.exports = router;
