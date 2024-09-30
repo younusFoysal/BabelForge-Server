72fbcead0c0789bdeab737700051488703129900
younusFoysal <younus.faysal@gmail.com>
2024-10-01 04:15:53 +0600

    get my teams API fix

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
+      "http://localhost:3001",
     "https://babel-forge.vercel.app",
   ],
   credentials: true,
