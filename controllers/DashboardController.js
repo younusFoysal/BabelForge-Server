7c30e3e816eb900c19476dfa6250d6ff888f7925
morshidulrahman <boydanger416@gmail.com>
2024-10-02 15:13:37 +0600

    dashboard api

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -7,23 +7,43 @@ const allDashinfo = async (req, res) => {
   const db = req.app.locals.db;
   const teamCollection = getTeamsCollection(db);
   const tasksCollection = getTasksCollection(db);
-  const teams = await teamCollection.find().toArray();
-  const tasks = await tasksCollection.find().toArray();
+
+  const email = req.params.email;
+  const query = { tmembers: email };
+  const taskquery = { author: email };
+  const team = await teamCollection.find(query).toArray();
+
+  const tasks = await tasksCollection.find(taskquery).toArray();
 
   const pendingTasks = tasks.filter(
     (task) => task.tproces === "inProgress" || task.tproces === "todo"
-  );
+  ).length;
+  const TodoTasks = tasks.filter((task) => task.tproces === "todo").length;
+  const DoneTask = tasks.filter((task) => task.tproces === "done").length;
+  const InprogressTask = tasks.filter(
+    (task) => task.tproces === "inProgress"
+  ).length;
 
-  const totalTeamMembers = teams.reduce(
+  const totalTeamMembers = team.reduce(
     (sum, team) => sum + team.tmembers.length,
     0
   );
 
-  const totalTeams = teams.length;
-  const totalTasks = tasks.length;
-  const totalPendingTasks = pendingTasks.length;
+  const newmamber = [...new Set(team.flatMap((t) => t.tmembers))];
 
-  res.send({ totalTeams, totalTasks, totalPendingTasks, totalTeamMembers });
+  const totalTeams = team.length;
+  const totalTasks = tasks.length;
+
+  res.send({
+    totalTeams,
+    totalTasks,
+    pendingTasks,
+    totalTeamMembers,
+    TodoTasks,
+    DoneTask,
+    InprogressTask,
+    newmamber,
+  });
 };
 
 module.exports = { allDashinfo };
