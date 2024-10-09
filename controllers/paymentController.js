4b4b70116129d95319d6b2a95675f7149f1657ad
Shikder Tarek <shikdertarek2019@gmail.com>
2024-10-09 13:58:12 +0600

    Add: getallpayment function

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,11 +1,18 @@
 
+const { getPaymentCollection } = require("../models/paymentModel");
 const { addPaymentService } = require("../services/paymentService");
-
+const db = req.app.locals.db;
 const addPayment = async(req,res)=>{
-    const db = req.app.locals.db;
+    
     const payment = req.body;
     const result = await addPaymentService(db, payment);
     res.send(result);
 }
 
-module.exports= {addPayment}
+const getAllPayment = async(req,res)=>{
+    const paymentCollection = getPaymentCollection(db);
+    const result = await paymentCollection.find().toArray();
+    res.send(result);
+}
+
+module.exports= {addPayment, getAllPayment}
