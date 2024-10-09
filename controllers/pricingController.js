c625a89726b0780d1206a189a74884dce1ee69bf
Shikder Tarek <shikdertarek2019@gmail.com>
2024-10-09 13:44:26 +0600

    add: update price function

--- controllers/pricingController.js
+++ controllers/pricingController.js
@@ -1,4 +1,5 @@
 
+const { ObjectId } = require("mongodb");
 const { getPricingCollection } = require("../models/pricingModel");
 
 
@@ -9,6 +10,29 @@ const getAllPrice = async(req,res)=>{
     res.send(result);
 }
 
+const updatePrice = async(req,res)=>{
+    db = req.app.locals.db;
+    const id = req.params.id;
+    const priceCollection = getPricingCollection(db);
+    const query = {_id: new ObjectId(id)};
+    const {title,price,priceDetails,featuresTitle,features,buttonText} = req.body;
+
+    const update = {
+        $set: {
+          ...(title && { title }),            
+          ...(price && { price }),              
+          ...(priceDetails && { priceDetails }), 
+          ...(featuresTitle && { featuresTitle }),
+          ...(features && { features }),        
+          ...(buttonText && { buttonText }),     
+        }
+      };
+
+      const result = await priceCollection.updateOne(query, update);
+      res.send(result)
+
+}
+
 module.exports={
-    getAllPrice
+    getAllPrice, updatePrice
 }
