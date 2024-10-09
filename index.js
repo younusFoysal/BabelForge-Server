9fb816e551518107b7e5aecca6a2443eaeccb0c2
morshidulrahman <boydanger416@gmail.com>
2024-10-09 11:27:10 +0600

    video

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ const port = process.env.PORT || 5000;
 
 // Middleware and CORS setup
 const corsOptions = {
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "https://babel-forge.vercel.app",
-    ],
-    credentials: true,
-    optionSuccessStatus: 200,
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://babel-forge.vercel.app",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -36,28 +36,28 @@ app.use(express.json());
 app.use(cookieParser());
 
 connectToDatabase()
-    .then((db) => {
-        app.locals.db = db;
-        console.log("Connected to MongoDB");
+  .then((db) => {
+    app.locals.db = db;
+    console.log("Connected to MongoDB");
 
-        app.use("/api", userRoutes);
-        app.use("/task", taskRoutes);
-        app.use("/team", teamRoutes);
-        app.use("/project", projectRoute);
-        app.use("/message", messageRoute);
-        app.use("/dashboard", dashRoute);
-        app.use("/chat", chatRoutes);
+    app.use("/api", userRoutes);
+    app.use("/task", taskRoutes);
+    app.use("/team", teamRoutes);
+    app.use("/project", projectRoute);
+    app.use("/message", messageRoute);
+    app.use("/dashboard", dashRoute);
+    app.use("/chat", chatRoutes);
 
-        app.get("/", (req, res) => {
-            res.send("Babel Server is Running...");
-        });
+    app.get("/", (req, res) => {
+      res.send("Babel Server is Running...");
+    });
 
-        // Initialize Socket.IO with the database
-        initSocket(server, db);
+    // Initialize Socket.IO with the database
+    initSocket(server, db);
 
-        // Start server
-        server.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
-    })
-    .catch((err) => console.error("Error connecting to MongoDB", err));
+    // Start server
+    server.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => console.error("Error connecting to MongoDB", err));
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { verifyToken, verifyAdmin, verifyHost } = require('../middleware/auth');
+const { verifyToken, verifyAdmin, verifyHost } = require("../middleware/auth");
 const {
-    createJwtToken,
-    logout,
-    createPaymentIntent,
-    addUser,
-    updateUserProfile,
-    getUser,
-    getAllUsers
-} = require('../controllers/userController');
+  createJwtToken,
+  logout,
+  createPaymentIntent,
+  addUser,
+  updateUserProfile,
+  getUser,
+  getAllUsers,
+} = require("../controllers/userController");
 
-router.post('/jwt', createJwtToken);
-router.get('/logout', logout);
-router.post('/create-payment-intent', verifyToken, createPaymentIntent);
-router.post('/users/add', addUser);
-router.put('/user', updateUserProfile);
-router.get('/user/:email', getUser);
-router.get('/users', getAllUsers);
-router.patch('/users/update/:email', updateUserProfile);
+router.post("/jwt", createJwtToken);
+router.get("/logout", logout);
+router.post("/create-payment-intent", verifyToken, createPaymentIntent);
+router.post("/users/add", addUser);
+router.put("/user", updateUserProfile);
+router.get("/user/:email", getUser);
+router.get("/users", getAllUsers);
+router.patch("/users/update/:email", updateUserProfile);
 
 module.exports = router;
