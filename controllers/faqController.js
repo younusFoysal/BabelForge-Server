870c286324b2d84b02175cf0cef94b6dacfe2461
nhnaahid <nhnahid.iu@gmail.com>
2024-10-31 02:03:46 +0600

    update: faq controller and model

--- controllers/faqController.js
+++ controllers/faqController.js
@@ -1,7 +1,9 @@
+const { getFaqCollection } = require("../models/faqModel");
+
 // Get all created teams.
 const getAllFaqs = async (req, res) => {
     const db = req.app.locals.db;
-    const teamCollection = getTeamsCollection(db);
+    const teamCollection = getFaqCollection(db);
     const result = await teamCollection.find().toArray();
     res.send(result);
 };
--- routes/faqRoutes.js
+++ routes/faqRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { getAllFaqs } = require('../controllers/faqController');
 const router = express.Router();
 
 // Get all faqs
