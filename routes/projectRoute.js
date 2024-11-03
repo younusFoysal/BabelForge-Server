488f7754fb8212f90a04d89f3512e99a32ae0c77
morshidulrahman <boydanger416@gmail.com>
2024-11-03 21:54:21 +0600

    verify token

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -13,19 +13,16 @@ const { verifyToken } = require("../middleware/auth");
 
 router.post("/projects", addPoject);
 
-// Get all projects GET /api/projects
 router.get("/projects", verifyToken, getProjects);
 
 router.get("/projects/single/:id", verifyToken, getsingleProject);
 
-// Get project members
 router.get("/projects/members/:id", verifyToken, getProjectMembers);
 
 router.get("/projects/my-projects", verifyToken, getMyProjects);
 
 router.delete("/projects/:id", verifyToken, deleteProject);
 
-// projects Update Api
 router.patch("/projects/update/:id", verifyToken, updateProject);
 
 module.exports = router;
